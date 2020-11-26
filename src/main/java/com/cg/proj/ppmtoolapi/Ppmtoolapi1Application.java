fe5365900e70f21e0174dec2ac74d425e2ea49e5
Dharu-98 <dharani.n2016csea@sece.ac.in>
2020-11-26 23:33:37 +0530

    initial commit

---
+++ src/main/java/com/cg/proj/ppmtoolapi/Ppmtoolapi1Application.java
@@ -0,0 +1,13 @@
+package com.cg.proj.ppmtoolapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ppmtoolapi1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ppmtoolapi1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/cg/proj/ppmtoolapi/Ppmtoolapi1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cg.proj.ppmtoolapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ppmtoolapi1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
